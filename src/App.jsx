ef83819f5d3d5efafcc162e29410d92c130f8fc0
DaniK <danikordonezgarcia@gmail.com>
2023-10-13 17:22:47 +0200

    add Gallery component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Header from "./components/Header"
+import Gallery from "./components/Gallery"
+// import InfoProduct from "./components/InfoProduct"
 
 export default function App() {
 
   return (
     <div >
       <Header />
+      <main className="grid content-center grid-cols-2">
+        <Gallery />
+        {/* <InfoProduct /> */}
+      </main>
     </div>
   )
 }
