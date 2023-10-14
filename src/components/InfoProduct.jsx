0f5e29ea83e60fe378f2950a3791da3bffa20bc5
DaniK <danikordonezgarcia@gmail.com>
2023-10-14 13:54:06 +0200

    add infoProduct component

---
+++ src/components/InfoProduct.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+
+import IconCart from '../assets/icon-cart.svg'
+import IconPlus from '../assets/icon-plus.svg'
+import IconMinus from '../assets/icon-minus.svg'
+
+export default function InfoProduct() {
+  const [quantity, setQuantity] = useState(1)
+
+  const handleQuantity = (num) => {
+    const newQuantity = quantity + num
+    if (newQuantity < 1) return
+    setQuantity(newQuantity)
+  }
+
+  return (
+    <article className="flex flex-col justify-center px-16 py-20">
+      <span className="py-5 font-bold text-orange-400">SNEAKER COMPANY</span>
+      <h1 className="text-5xl font-bold">Fall Limited Edition Sneakers</h1>
+      <p className="py-8 text-gray-500">
+        These low-profile sneakers are you perfect casual wear
+        companion. Featuring a durable rubber outer sole, they&apos;ll
+        withstand everything the weather can offer.
+      </p>
+      <div className="flex items-center gap-5 pb-3 text-3xl font-bold">
+        <span>$125.00</span>
+        <span className="py-[0.1rem] px-[0.6rem] text-base text-orange-400 bg-orange-100 rounded-md">50%</span>
+      </div>
+      <span className="font-bold text-gray-400 line-through">$250.00</span>
+
+      <footer className="flex items-center gap-4 py-10">
+        <div className="flex items-center h-full bg-gray-100 rounded-md">
+          <button className="flex items-center justify-center w-12 h-full text-white rounded-md" onClick={() => handleQuantity(-1)}><img src={IconMinus} alt="icon minus" /></button>
+            <span className="flex items-center justify-center w-12 font-bold ">{quantity}</span>
+          <button className="flex items-center justify-center w-12 h-full text-white rounded-md" onClick={() => handleQuantity(1)}><img src={IconPlus} alt="icon plus" /></button>
+        </div>
+        <button className="flex items-center justify-center w-full gap-4 py-4 text-white bg-orange-400 rounded-xl hover:bg-orange-300 fill-white">
+          <img src={IconCart} alt="icon cart" className='fill-white'/>
+          <span className='font-bold'>Add to cart</span>
+        </button>
+      </footer>
+    </article>
+  )
+}
