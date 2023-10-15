7b2d5afc9d4edf0d6fd5ea8062dc9613f42a05fb
DaniK <danikordonezgarcia@gmail.com>
2023-10-15 21:15:15 +0200

    add CartProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import Header from "./components/Header"
-import Gallery from "./components/Gallery"
-import InfoProduct from "./components/InfoProduct"
+import Header from './components/Header'
+import Gallery from './components/Gallery'
+import InfoProduct from './components/InfoProduct'
+import { CartProvider } from './context/cart'
 
 export default function App() {
   return (
-    <>
+    <CartProvider>
       <Header />
       <main className="grid content-center grid-cols-1 lg:grid-cols-2">
         <Gallery />
         <InfoProduct />
       </main>
-    </>
+    </CartProvider>
   )
 }
