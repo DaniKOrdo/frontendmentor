40e641b6c2f4726ce1689a8beabdc9b0dbfb8473
DaniK <danikordonezgarcia@gmail.com>
2023-10-15 21:19:56 +0200

    add context and product object

--- src/components/InfoProduct.jsx
+++ src/components/InfoProduct.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
 
-import IconCart from '../assets/icon-cart.svg'
+import { USDollarPrice } from '../utils/format'
+import IconCart from '../assets/icon-cart-white.svg'
 import IconPlus from '../assets/icon-plus.svg'
 import IconMinus from '../assets/icon-minus.svg'
+import { useCart } from '../hooks/useCart'
+
+const product = {
+  id: 1,
+  name: 'Fall Limited Edition Sneakers',
+  price: 125.00,
+  image: 'image-product-1-thumbnail.jpg'
+}
 
 export default function InfoProduct() {
   const [quantity, setQuantity] = useState(1)
+  const { addToCart } = useCart()
 
   const handleQuantity = (num) => {
     const newQuantity = quantity + num
@@ -23,19 +33,19 @@ export default function InfoProduct() {
         withstand everything the weather can offer.
       </p>
       <div className="flex items-center gap-5 pb-3 text-3xl font-bold">
-        <span>$125.00</span>
+        <span>{USDollarPrice(125)}</span>
         <span className="py-[0.1rem] px-[0.6rem] text-base text-orange-400 bg-orange-100 rounded-md">50%</span>
       </div>
-      <span className="font-bold text-gray-400 line-through">$250.00</span>
+      <span className="font-bold text-gray-400 line-through">{USDollarPrice(250)}</span>
 
       <footer className="flex items-center gap-4 py-10">
         <div className="flex items-center h-full bg-gray-100 rounded-md">
           <button className="flex items-center justify-center w-12 h-full text-white rounded-md" onClick={() => handleQuantity(-1)}><img src={IconMinus} alt="icon minus" /></button>
-            <span className="flex items-center justify-center w-12 font-bold ">{quantity}</span>
+          <span className="flex items-center justify-center w-12 font-bold ">{quantity}</span>
           <button className="flex items-center justify-center w-12 h-full text-white rounded-md" onClick={() => handleQuantity(1)}><img src={IconPlus} alt="icon plus" /></button>
         </div>
-        <button className="flex items-center justify-center w-full gap-4 py-4 text-white bg-orange-400 rounded-xl hover:bg-orange-300 fill-white">
-          <img src={IconCart} alt="icon cart" className='fill-white'/>
+        <button className="flex items-center justify-center w-full gap-4 py-4 text-white bg-orange-400 rounded-xl hover:bg-orange-300 fill-white" onClick={() => addToCart(product, quantity)}>
+          <img src={IconCart} alt="icon cart" className='w-5' />
           <span className='font-bold'>Add to cart</span>
         </button>
       </footer>
