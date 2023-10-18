415dfe43792dedd92f4e3ff8fdafce8773bf7465
DaniK <danikordonezgarcia@gmail.com>
2023-10-18 20:17:47 +0200

    add mobile arrows

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -24,15 +24,48 @@ export default function Gallery() {
     setIsModalOpen(false);
   };
 
+  const getPreviousImage = () => {
+    const currentIndex = images.findIndex(image => image.id === currentImage);
+    const previousIndex = (currentIndex - 1 + images.length) % images.length;
+    return images[previousIndex].id;
+  };
+
+  const getNextImage = () => {
+    const currentIndex = images.findIndex(image => image.id === currentImage);
+    const nextIndex = (currentIndex + 1) % images.length;
+    return images[nextIndex].id;
+  };
+
   return (
-    <aside className='flex flex-col gap-8 px-16 py-20 md:px-16 md:py-20'>
+    <aside className='flex flex-col gap-8 md:px-16 md:py-20'>
+      <div className='relative md:hidden'>
+        <button
+          className="absolute flex items-center justify-center w-14 h-14 transform rotate-180 bg-white rounded-full left-4 top-[45%]"
+          onClick={() => handleThumbnailClick(getPreviousImage())}
+        >
+          <img src="src/assets/icon-next.svg" alt='Back Icon' />
+        </button>
+        <img
+          className=''
+          src={`${currentImage}.jpg`}
+          alt="Sneakers Image"
+        />
+
+        <button
+          className="absolute flex items-center justify-center w-14 h-14 bg-white rounded-full right-4 top-[45%]"
+          onClick={() => handleThumbnailClick(getNextImage())}
+        >
+          <img src="src/assets/icon-next.svg" alt='Next Icon' />
+        </button>
+      </div>
+
       <img
-        className='cursor-pointer rounded-2xl'
+        className='hidden cursor-pointer rounded-2xl md:block'
         src={`${currentImage}.jpg`}
         alt="Sneakers Image"
         onClick={openModal}
       />
-      <div className='flex justify-between gap-4'>
+      <div className='justify-between hidden gap-4 md:flex'>
         {images.map(({ id, src, alt }) => (
           <div key={id} className={`bg-white w-1/5 rounded-xl ${currentImage === id && 'ring-2 ring-orange-400'}`}>
             <img
