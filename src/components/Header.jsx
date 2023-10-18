bf93684f3fac6d79cadfcb624aa099e5791a773c
DaniK <danikordonezgarcia@gmail.com>
2023-10-18 20:18:05 +0200

    add mobile menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,39 +8,62 @@ import IconMenu from '../assets/icon-menu.svg'
 import Avatar from '../assets/image-avatar.png'
 
 export default function Header() {
+  const [showMenu, setShowMenu] = useState(false)
   const [showCart, setShowCart] = useState(false)
   const { getTotalItems } = useCart()
 
-  return (
-    <nav className="relative mx-auto px-2 pb-8 lg:mx-6 border-b-[1px]">
+   return (
+    <nav className="relative mx-auto md:px-2 px-6 md:pb-8 py-2 lg:mx-6 border-b-[1px]">
       <div className="flex items-center justify-between h-16">
-
         <div className="inset-y-0 left-0 flex items-center lg:hidden">
-          <button className="inline-flex items-center justify-center text-gray-400 hover:text-gray-500">
-            <img src={IconMenu} alt="icon menu" />
+          <button className="inline-flex items-center justify-center ml-2 text-gray-400 hover:text-gray-500" onClick={() => setShowMenu(!showMenu)}>
+            <img src={IconMenu} alt="icon menu" className="w-5" />
           </button>
         </div>
 
+        <div className={`fixed inset-0 z-50 w-screen h-screen bg-black bg-opacity-75 transition-transform ${showMenu ? 'transform translate-x-0' : 'transform -translate-x-full bg-opacity-0'}`}>
+          <div className="flex flex-col items-start h-screen gap-6 p-8 bg-white w-80">
+            <button className="mb-8" onClick={() => setShowMenu(!showMenu)}>
+              <img src="src/assets/icon-close.svg" alt="icon close" className="w-5" />
+            </button>
+            <a href="#" className="text-2xl font-bold">
+              Collections
+            </a>
+            <a href="#" className="text-2xl font-bold">
+              Men
+            </a>
+            <a href="#" className="text-2xl font-bold">
+              Women
+            </a>
+            <a href="#" className="text-2xl font-bold">
+              About
+            </a>
+            <a href="#" className="text-2xl font-bold">
+              Contact
+            </a>
+          </div>
+        </div>
+
         <div className="flex items-center justify-center lg:items-stretch lg:justify-start">
-          <span className="flex items-center ml-8 text-4xl font-bold lg:ml-0">
-            <img src={Logo} alt="icon cart" />
+          <span className="flex items-center text-4xl font-bold md:mr-4 md:ml-8 lg:ml-0">
+            <img src={Logo} alt="sneakers logo" />
           </span>
           <div className="hidden lg:ml-6 lg:block">
             <div className="flex px-3 space-x-8 font-medium text-gray-500 text-md">
-              <a href="#" className="hover:text-gray-800 group">
-                Collections <span className="w-full h-1 bg-orange-500 top-[72px] left-0 transform scale-x-0 group-hover:scale-x-100 transition-transform"></span>
+              <a href="#" className="relative hover:text-gray-800 group">
+                Collections <span className="absolute w-full h-1 bg-orange-500 top-[72px] left-0 transform scale-x-0 group-hover:scale-x-100 transition-transform"></span>
               </a>
-              <a href="#" className="hover:text-gray-800 group">
-                Men <span className="w-full h-1 bg-orange-500 top-[72px] left-0 transform scale-x-0 group-hover:scale-x-100 transition-transform"></span>
+              <a href="#" className="relative hover:text-gray-800 group">
+                Men <span className="absolute w-full h-1 bg-orange-500 top-[72px] left-0 transform scale-x-0 group-hover:scale-x-100 transition-transform"></span>
               </a>
-              <a href="#" className="hover:text-gray-800 group">
-                Women <span className="w-full h-1 bg-orange-500 top-[72px] left-0 transform scale-x-0 group-hover:scale-x-100 transition-transform"></span>
+              <a href="#" className="relative hover:text-gray-800 group">
+                Women <span className="absolute w-full h-1 bg-orange-500 top-[72px] left-0 transform scale-x-0 group-hover:scale-x-100 transition-transform"></span>
               </a>
-              <a href="#" className="hover:text-gray-800 group">
-                About <span className="w-full h-1 bg-orange-500 top-[72px] left-0 transform scale-x-0 group-hover:scale-x-100 transition-transform"></span>
+              <a href="#" className="relative hover:text-gray-800 group">
+                About <span className="absolute w-full h-1 bg-orange-500 top-[72px] left-0 transform scale-x-0 group-hover:scale-x-100 transition-transform"></span>
               </a>
-              <a href="#" className="hover:text-gray-800 group">
-                Contact <span className="w-full h-1 bg-orange-500 top-[72px] left-0 transform scale-x-0 group-hover:scale-x-100 transition-transform"></span>
+              <a href="#" className="relative hover:text-gray-800 group">
+                Contact <span className="absolute w-full h-1 bg-orange-500 top-[72px] left-0 transform scale-x-0 group-hover:scale-x-100 transition-transform"></span>
               </a>
             </div>
           </div>
@@ -58,7 +81,7 @@ export default function Header() {
 
           <div className="ml-3">
             <button className="flex rounded-full hover:ring-2 hover:ring-orange-400">
-              <img className="h-12 rounded-full" src={Avatar} alt="profile picture" />
+              <img className="h-8 rounded-full md:h-12" src={Avatar} alt="profile picture" />
             </button>
           </div>
         </div>
