a0ce2e5004c6bb8a425481bb2e2310a8e773bb75
DaniK <danikordonezgarcia@gmail.com>
2023-10-21 18:10:00 +0200

    add custom tailwind color

--- Age-Calculator-App/tailwind.config.js
+++ Age-Calculator-App/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'customPurple': 'hsl(259, 100%, 65%)',
+      },
+    },
   },
   plugins: [],
 }
