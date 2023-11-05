79e107d5961feb5e415059665cd908048ec6ae40
DaniK <danikordonezgarcia@gmail.com>
2023-11-05 21:59:05 +0100

    Add projects data to src/data/projects.js

---
+++ src/data/projects.js
@@ -0,0 +1,22 @@
+export const projects = [
+  {
+    img: "public/age-calculator-preview.jpg",
+    title: "Age calculator app",
+    tags: ["react", "tailwind"],
+    level: "Junior",
+    description: "This challenge is designed to sharpen your JavaScript and form validation skills. Working with dates in JavaScript can be tricky, so this will be a nice test!.",
+    linkDemo: "https://age-calculator.danik.dev/",
+    linkGithub: "https://github.com/DaniKOrdo/frontendmentor/tree/main/Age-Calculator-App",
+    linkFrontendMentor: "https://www.frontendmentor.io/solutions/react-vite-tailwind-ecommerce-product-page-cOKOmwYOZN",
+  },
+  {
+    img: "public/product-page-preview.jpg",
+    title: "E-commerce product page",
+    tags: ["react", "tailwind"],
+    level: "Intermediate",
+    description: "In this challenge, you'll build a beautiful product page. We'll be putting your JS skills to the test with a lightbox product gallery and cart functionality!",
+    linkDemo: "https://product-page.danik.dev/",
+    linkGithub: "https://github.com/DaniKOrdo/frontendmentor/tree/main/E-Commerce-Product-Page",
+    linkFrontendMentor: "https://www.frontendmentor.io/solutions/react-vite-tailwind-ecommerce-product-page-cOKOmwYOZN",
+  }
+]
