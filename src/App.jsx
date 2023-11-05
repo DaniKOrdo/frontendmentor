c397ea264aee8fb5f91aa07ab7963cf0d4f291a2
DaniK <danikordonezgarcia@gmail.com>
2023-11-05 21:58:48 +0100

    Refactor import paths and update project data

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,11 @@
-import { DKLogo, FrontendMentor, X } from "./assets/Icons"
+import { DKLogo, FrontendMentor, X } from "./utils/Icons"
 import Project from "./components/Project"
-
-const projects = [
-  {
-    img: "https://www.frontendmentor.io/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdz209s6jk%2Fimage%2Fupload%2Fv1680193709%2FChallenges%2Fmwjogovjjnkz6f6yzdkp.jpg&w=384&q=75",
-    title: "Age calculator app",
-    tags: ["react", "vite", "tailwind"],
-    number: "2",
-    level: "Junior",
-    description: "This challenge is designed to sharpen your JavaScript and form validation skills. Working with dates in JavaScript can be tricky, so this will be a nice test!.",
-    linkDemo: "",
-    linkGithub: "",
-    linkFrontendMentor: "https://www.frontendmentor.io/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdz209s6jk%2Fimage%2Fupload%2Fv1680193709%2FChallenges%2Fmwjogovjjnkz6f6yzdkp.jpg&w=384&q=75",
-  },
-  {
-    img: "https://www.frontendmentor.io/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdz209s6jk%2Fimage%2Fupload%2Fv1633619397%2FChallenges%2Ffhzpdnabrek50hvhftnl.jpg&w=384&q=75",
-    title: "E-commerce product page",
-    tags: ["react", "vite", "tailwind"],
-    number: "3",
-    level: "Intermediate",
-    description: "In this challenge, you'll build a beautiful product page. We'll be putting your JS skills to the test with a lightbox product gallery and cart functionality!",
-    linkDemo: "",
-    linkGithub: "",
-    linkFrontendMentor: "https://www.frontendmentor.io/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdz209s6jk%2Fimage%2Fupload%2Fv1680193709%2FChallenges%2Fmwjogovjjnkz6f6yzdkp.jpg&w=384&q=75",
-  },
-  {
-    img: "https://www.frontendmentor.io/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdz209s6jk%2Fimage%2Fupload%2Fv1680193709%2FChallenges%2Fmwjogovjjnkz6f6yzdkp.jpg&w=384&q=75",
-    title: "Test",
-    tags: ["react", "vite", "tailwind"],
-    number: "2",
-    level: "Advanced",
-    description: "This challenge is designed to sharpen your JavaScript and form validation skills. Working with dates in JavaScript can be tricky, so this will be a nice test!.",
-    linkDemo: "",
-    linkGithub: "",
-    linkFrontendMentor: "https://www.frontendmentor.io/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdz209s6jk%2Fimage%2Fupload%2Fv1680193709%2FChallenges%2Fmwjogovjjnkz6f6yzdkp.jpg&w=384&q=75",
-  },
-  {
-    img: "https://www.frontendmentor.io/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdz209s6jk%2Fimage%2Fupload%2Fv1680193709%2FChallenges%2Fmwjogovjjnkz6f6yzdkp.jpg&w=384&q=75",
-    title: "Age calculator app",
-    tags: ["react", "vite", "tailwind"],
-    number: "2",
-    level: "Intermediate",
-    description: "This challenge is designed to sharpen your JavaScript and form validation skills. Working with dates in JavaScript can be tricky, so this will be a nice test!.",
-    linkDemo: "",
-    linkGithub: "",
-    linkFrontendMentor: "https://www.frontendmentor.io/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdz209s6jk%2Fimage%2Fupload%2Fv1680193709%2FChallenges%2Fmwjogovjjnkz6f6yzdkp.jpg&w=384&q=75",
-  },
-]
+import { projects } from "./data/projects.js"
 
 function App() {
   return (
     <>
-      <header className="flex items-center gap-4 my-7">
+      <header className="flex items-center gap-4 my-8 md:my-16">
         <a href="https://www.frontendmentor.io/profile/DaniKOrdo" target="_blank" rel="noopener noreferrer">
           <FrontendMentor />
         </a>
@@ -61,7 +15,7 @@ function App() {
         </a>
       </header>
 
-      <main className="grid max-w-6xl grid-cols-1 gap-16 p-4 md:gap-4 md:mt-10 xl md:grid-cols-2 lg:grid-cols-3">
+      <main className="grid max-w-6xl grid-cols-1 gap-16 p-4 mb-32 md:p-0 md:gap-4 md:mt-10 xl md:grid-cols-2 lg:grid-cols-3">
         {projects.map((project, index) => (
           <Project key={index} {...project} />
         ))}
